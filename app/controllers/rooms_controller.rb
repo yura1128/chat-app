0e66a495b6aa7ae5d4811bae79fc8523fb7302f2
yura1128 <yumeto1128.pr@gmail.com>
2023-11-02 17:01:30 +0900

    画面遷移の実装

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,5 +1,8 @@
 class RoomsController < ApplicationController
 
+  def index
+  end
+
   def new
     @room = Room.new
   end
@@ -18,5 +21,5 @@ class RoomsController < ApplicationController
   def room_params
     params.require(:room).permit(:name, user_ids: [])
   end
-  
+
 end
